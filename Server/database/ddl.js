90ae450941b6c2445a46a0be23b0945a27cb17de
Kristian Kryger <kristianlaukryger@gmail.com>
2023-04-05 20:32:08 +0200

    DB creation and connection has been done

--- Server/database/ddl.js
+++ Server/database/ddl.js
@@ -17,12 +17,25 @@ db.execute(`CREATE TABLE IF NOT EXISTS users(
     admin BOOLEAN NOT NULL
 );`);
 
+db.execute(`CREATE TABLE IF NOT EXISTS ninjas(
+    ninja_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR (255) NOT NULL,
+    age INTEGER NOT NULL,
+    nation VARCHAR(255) NOT NULL,
+    jutsu VARCHAR (255) NOT NULL,
+    hokage BOOLEAN NOT NULL,
+    user_id INTEGER NOT NULL,
+    CONSTRAINT fk_users_ninja FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);`); 
+
 if (isInDeleteMode) {
     //users
     db.execute(`INSERT INTO users(mail, user_name, password, admin) VALUES (?, ?, ?, ?);`, ["bingo@bongo.com", "Minato", "123", true]);
-    db.execute(`INSERT INTO users(mail, user_name, password, admin) VALUES (?, ?, ?, ?);`, ["bingo@bongo.com", "Sasuke", "123", false]);
-    db.execute(`INSERT INTO users(mail, user_name, password, admin) VALUES (?, ?, ?, ?);`, ["bingo@bongo.com", "Naruto", "123", false]);
+    db.execute(`INSERT INTO users(mail, user_name, password, admin) VALUES (?, ?, ?, ?);`, ["bingo@spongo.com", "Sasuke", "123", false]);
+    db.execute(`INSERT INTO users(mail, user_name, password, admin) VALUES (?, ?, ?, ?);`, ["bingo@flongo.com", "Naruto", "123", false]);
+
+    db.execute(`INSERT INTO ninjas(name, age, nation, jutsu, hokage, user_id) VALUE (?, ?, ?, ?, ?, ?);`, ["Zuko", 69, "Fire", "Fireball", false, 1]);
 
 }
 
-db.end()
+db.end();
